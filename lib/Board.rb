efec08d50c1e942feebac095b379fd16572ab583
Robert Groller <robertgroller@gmail.com>
2014-08-07 15:40:17 -0400

    Updates to board and ship class

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,20 +1,23 @@
 require 'pry'
 require_relative './Ship.rb'
+require 'YAML'
 
 class Board
-  def initialize(size=5)
-    @ships = []
-    @board = []
-    size.times do
-      @board << []
-    end
-    @board.each do |array|
-      size.times do
-        array.push " "
+  def initialize(board_file)
+    @board_hash = YAML.load(board_file) 
+    @active_pos = {}
+    @inactive_pos = {}
+    @misses = []
+  end
+
+  def creat_pos
+    @board_hash.each do |name, pos_arr| 
+      ship = Ship.new(name)
+      pos_arr.each do |pos|
+        @active_pos[pos] = ship
       end
     end
   end
-
 
   def add_ship(length=Rand(1..4))
     ship = Ship.new(length)
