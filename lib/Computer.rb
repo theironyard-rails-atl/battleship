f8f18179acfd9cc34106e87b873170736462ebe7
Taylor Martin <taylormartin@Howard.local>
2014-08-07 17:53:04 -0400

    "made the participant, computer, and player classes all one thing. also added fire to the player and computer classes."

--- lib/Computer.rb
+++ lib/Computer.rb
@@ -1,11 +1,4 @@
 require 'pry'
 require_relative './Participant.rb'
 
-class Computer < Participant
-
-  def initialize(level=1)
-    @level = level
-    super()
-  end
-
-end
+#everything is now in participant
--- lib/Player.rb
+++ lib/Player.rb
@@ -1,12 +1,4 @@
 require 'pry'
 require_relative './Participant.rb'
 
-class Player < Participant
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-    super()
-  end
-  
-end
+#everything is now in participant
